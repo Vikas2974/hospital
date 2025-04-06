6a019d3428b09d980f2ce20a0d14b9cb5613ebfe
Vikas Namdev <itsvikas2974@gmail.com>
2025-04-06 14:36:04 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,55 @@
 import React, { useContext, useEffect } from "react";
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./components/Dashboard";
-import Login from "./components/Login";
-import AddNewDoctor from "./components/AddNewDoctor";
-import Messages from "./components/Messages";
-import Doctors from "./components/Doctors";
-import { Context } from "./main";
-import axios from "axios";
+import Home from "./Pages/Home";
+import Appointment from "./Pages/Appointment";
+import AboutUs from "./Pages/AboutUs";
+import Register from "./Pages/Register";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Sidebar from "./components/Sidebar";
-import AddNewAdmin from "./components/AddNewAdmin";
-import "./App.css";
-
+import axios from "axios";
+import { Context } from "./main";
+import Login from "./Pages/Login";
 const App = () => {
-  const { isAuthenticated, setIsAuthenticated, admin, setAdmin } =
+  const { isAuthenticated, setIsAuthenticated, setUser } =
     useContext(Context);
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/admin/me",
+          "http://localhost:4000/api/v1/user/patient/me",
           {
             withCredentials: true,
           }
         );
         setIsAuthenticated(true);
-        setAdmin(response.data.user);
+        setUser(response.data.user);
       } catch (error) {
         setIsAuthenticated(false);
-        setAdmin({});
+        setUser({});
       }
     };
     fetchUser();
   }, [isAuthenticated]);
 
   return (
-    <Router>
-      <Sidebar />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/doctor/addnew" element={<AddNewDoctor />} />
-        <Route path="/admin/addnew" element={<AddNewAdmin />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/doctors" element={<Doctors />} />
-      </Routes>
-      <ToastContainer position="top-center" />
-    </Router>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/appointment" element={<Appointment />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+        <ToastContainer position="top-center" />
+      </Router>
+    </>
   );
 };
 
